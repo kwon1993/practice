de332cdb912ecc0c42082d1b22dbb2ae3a911d1a
wdmono1993 <wdmono@naver.com>
2019-09-10 23:33:13 +0900

    first commit

---
+++ social-server/src/main/java/com/github/prgrms/example/SocialServerApplication.java
@@ -0,0 +1,13 @@
+package com.github.prgrms.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialServerApplication.class, args);
+	}
+
+}
